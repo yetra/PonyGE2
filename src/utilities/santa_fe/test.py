696a4f12475d790b79064e254f893e8f1746cac0
Bruna <brdujmovic@gmail.com>
2019-06-14 00:57:12 +0200

    Add a program for displaying evolution results

---
+++ src/utilities/santa_fe/test.py
@@ -0,0 +1,38 @@
+from santa_fe.Ant import Ant
+from santa_fe.Trail import Trail
+
+
+def main():
+    """
+    Displays the results of an evolved Santa Fe Trail solution on a specified trail.
+    """
+    trail = Grid('/Users/boftherebellion/Desktop/seminar/PonyGE2/datasets/AntTrails/santa_fe_trail.txt')
+
+    print(str(trail))
+
+    d = {'ant': Ant(trail, 600)}
+
+    while not d['ant'].finished():
+        exec('''if ant.food_ahead():
+  ant.move()
+else:
+  ant.turn_right()
+  if ant.food_ahead():
+    ant.turn_left()
+  else:
+    ant.turn_right()
+    ant.turn_right()
+  if ant.food_ahead():
+    ant.move()
+    ant.move()
+  else:
+    ant.turn_right()
+    ant.move()
+''', d)
+
+    print(str(trail))
+    print(str(d['ant']))
+
+
+if __name__ == '__main__':
+    main()
